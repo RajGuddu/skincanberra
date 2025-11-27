35bbb625c8abdc7e5e43a822d6186cabd7a1b66c
RajGuddu <rajgudduara18@gmail.com>
2025-11-27 11:09:26 +0530

    new changes 2711

--- config/constant.php
+++ config/constant.php
@@ -1,8 +1,9 @@
 <?php 
- defined('ADMIN')   || define('ADMIN', 'admin');
- defined('ADMIN_MAIL_TO')   || define('ADMIN_MAIL_TO', 'test152@yopmail.com');
- defined('IMAGE_PATH')   || define('IMAGE_PATH', 'public/assets/uploads/images/');
+    defined('ADMIN')   || define('ADMIN', 'admin');
+    defined('ADMIN_MAIL_TO')   || define('ADMIN_MAIL_TO', 'test152@yopmail.com');
+    defined('IMAGE_PATH')   || define('IMAGE_PATH', 'public/assets/uploads/images/');
+    defined('STRIPE_KEY')   || define('STRIPE_KEY', env('STRIPE_KEY'));  
+    defined('STRIPE_SECRET')   || define('STRIPE_SECRET', env('STRIPE_SECRET'));  
 
-
- defined('STRIPE_CURRENCY')   || define('STRIPE_CURRENCY', 'USD');
+    defined('STRIPE_CURRENCY')   || define('STRIPE_CURRENCY', 'USD');
 
